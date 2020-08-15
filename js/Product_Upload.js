11194752f6eee6c65cb86164805d4c0f5811e0c0
Coding-Players <ataota.com@gmail.com>
2020-08-15 10:36:34 +0530

    Image preview added Version 1.00.03

--- js/Product_Upload.js
+++ js/Product_Upload.js
@@ -1,1 +1,20 @@
-console.log('This is product upload page');
+// Product Image Upload prevew
+const Product_Picture = document.getElementById("id_prod_picture");
+const previewContainer_Product_Picture = document.getElementById("Product_Upload_image_Preview");
+const previewImage_Product_pick = previewContainer_Product_Picture.querySelector(".Product_Upload_image_preview__image");
+const previewDefaultText_Product_pick = previewContainer_Product_Picture.querySelector(".Product_Upload_image_preview_default_text");
+
+Product_Picture.addEventListener("change", function(){
+    const file = this.files[0];
+
+    if (file){
+        const reader = new FileReader();
+
+        previewDefaultText_Product_pick.style.display = "none";
+        previewImage_Product_pick.style.display = "block";
+        reader.addEventListener("load", function(){
+            previewImage_Product_pick.setAttribute("src", this.result);
+        });
+        reader.readAsDataURL(file);
+    }
+});
